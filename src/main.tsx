e73375bf3d84062fd1e28732a238a4b64b1059b7
C Kale <chankale310@gmail.com>
2025-08-09 15:44:35 +0200

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,17 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import ResourcesPage from "@/pages/Resources";
+import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/resources", element: <ResourcesPage /> },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
